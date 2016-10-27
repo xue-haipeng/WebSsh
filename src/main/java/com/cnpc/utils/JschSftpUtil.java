140c7ef7282ac3629ac2da4ec2e9e002933c01c4
xuehaipeng <1071405234@qq.com>
2016-10-27 12:47:36 +0800

    fix bug of non-sap backup check

--- src/main/java/com/cnpc/utils/JschSftpUtil.java
+++ src/main/java/com/cnpc/utils/JschSftpUtil.java
@@ -6,6 +6,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class JschSftpUtil {
 
@@ -32,7 +36,19 @@ public class JschSftpUtil {
 			String line;
 			while ((line = br.readLine()) != null) {
 				if (line.contains("successfully")) {
-					lastLine = "succeed";
+					String dateStr = line.substring(27, 55);
+					System.out.println(dateStr);
+					Date today = new Date();
+					Date date = new Date();
+					SimpleDateFormat format = new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy");
+					try {
+						date = format.parse(dateStr);
+						if (date.after(getPreviousDate(today))) {
+							lastLine = "succeed";
+						}
+					} catch (ParseException e) {
+						e.printStackTrace();
+					}
 				} else if (line.contains("ended in error")) {
 					lastLine = "failed";
 				}
@@ -50,6 +66,14 @@ public class JschSftpUtil {
 
 	public static String getLastLine() {
 		return lastLine;
+	}
+
+	public static Date getPreviousDate(Date date) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date);
+		calendar.add(Calendar.DAY_OF_MONTH, -1);
+		date = calendar.getTime();
+		return date;
 	}
 
 }
